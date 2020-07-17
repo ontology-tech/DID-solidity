42abd0b07c9c4dece88b0d9f004ee576c2a9996a
qiluge <wangcheng@onchain.com>
2020-07-17 18:48:28 +0800

    add some contract

---
+++ contracts/ContentContracts.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.6.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+abstract contract ContentContract {
+    mapping(string => address) didData;
+
+    function put(string memory did, bytes memory data) public virtual;
+    function remove(string memory did, uint index) public virtual;
+    function update(string memory did, uint index, bytes memory data) public virtual;
+    function get(string memory did, uint inde) public virtual returns(bytes memory);
+}
